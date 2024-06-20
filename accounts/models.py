56181769b9fd334be394996f33b9077b4e9dad43
chetan9119 <chetan.parashar09@gmail.com>
2024-06-20 11:34:19 +0530

    SMTP configuration for link activation

--- accounts/models.py
+++ accounts/models.py
@@ -1,25 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
-
 class MyAccountManger(BaseUserManager):
-    
+
     def create_user(self,first_name,last_name,username,email,password=None):
-        
         if not email:
-            raise ValueError('User must have an email address')
-        
+            raise ValueError('User must have an email address')        
         if not username:
             raise ValueError('User must have username')
-        
         user = self.model(
             email = self.normalize_email(email),
             username = username,
             first_name = first_name,
             last_name = last_name,
         )
-
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -35,12 +29,9 @@ class MyAccountManger(BaseUserManager):
         user.is_admin = True
         user.is_staff = True
         user.is_active = True
-        user.is_superadmin = True
-        
+        user.is_superadmin = True        
         user.save(using=self._db)
         return user
-
-
 
 class Account(AbstractBaseUser):
     
--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,15 @@ from .models import *
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
-def register(request):
-    
+# verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+def register(request):  
     if request.method == "POST":
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -18,6 +25,21 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+            
+            # User Activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification.html', {
+                'user':user, # user object itself
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)), # to hide primary key of user
+                'token':default_token_generator.make_token(user),                
+            })
+            
+            to_email = email
+            send_email = EmailMessage(subject=mail_subject, body=message, to=[to_email])
+            send_email.send()
+            
             messages.success(request, 'Registration Successfull !')
             return redirect('register')
             
@@ -50,4 +72,8 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'Your are logged out.')
     return redirect('login')
+
+
+def activate(request):
+    return
 
